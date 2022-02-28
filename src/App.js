fbfc7cd6e39955ee45d9a8e45fe32a6fce8f5f64
Xar0n <dima.dima.maksimov1998@mail.ru>
2022-03-01 00:57:11 +0800

    авторизация и выход

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import './scss/style.scss'
+import axios from 'axios'
+
+const loading = (
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+)
+axios.create({
+  withCredentials: false,
+  headers: {
+    'Access-Control-Allow-Origin': true,
+  },
+})
+axios.defaults.baseURL = 'http://localhost:8000/'
+axios.defaults.headers.post['Accept'] = 'application/json'
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('auth_token')
+  config.headers.Authorization = token ? `Bearer ${token}` : ''
+  return config
+})
+
+// Containers
+const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
+
+// Pages
+const Login = React.lazy(() => import('./views/pages/login/Login'))
+const Register = React.lazy(() => import('./views/pages/register/Register'))
+const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
+const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <React.Suspense fallback={loading}>
+          <Switch>
+            <Route exact path="/login" name="Login Page" render={(props) => <Login {...props} />} />
+            <Route
+              exact
+              path="/register"
+              name="Register Page"
+              render={(props) => <Register {...props} />}
+            />
+            <Route exact path="/404" name="Page 404" render={(props) => <Page404 {...props} />} />
+            <Route exact path="/500" name="Page 500" render={(props) => <Page500 {...props} />} />
+            <Route path="/" name="Home" render={(props) => <DefaultLayout {...props} />} />
+          </Switch>
+        </React.Suspense>
+      </HashRouter>
+    )
+  }
+}
+
+export default App
---
+++ src/views/pages/login/Login.js
@@ -0,0 +1,125 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+import axios from 'axios'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilLockLocked, cilUser } from '@coreui/icons'
+import Select from 'react-select'
+import Swal from 'sweetalert2'
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+]
+
+const Login = () => {
+  const history = useHistory()
+  const [selectedOption, setSelectedOption] = useState(null)
+  const [loginInput, setLogin] = useState({
+    login: '',
+    password: '',
+    error_list: [],
+  })
+
+  const handleInput = (e) => {
+    e.persist()
+    setLogin({ ...loginInput, [e.target.name]: e.target.value })
+  }
+
+  const loginSubmit = (e) => {
+    e.preventDefault()
+    const data = {
+      name: loginInput.name,
+      password: loginInput.password,
+    }
+    axios.get('/sanctum/csrf-cookie').then(() => {
+      axios.post('/api/login', data).then((res) => {
+        if (res.data.status === 200) {
+          localStorage.setItem('auth_token', res.data.token)
+          localStorage.setItem('auth_name', res.data.username)
+          Swal.fire('Авторизация', res.data.message, 'success')
+          history.push('/')
+        } else if (res.data.status === 401) {
+          Swal.fire('Авторизация', res.data.message, 'warning')
+        } else {
+          setLogin({ ...loginInput, error_list: res.data.validation_errors })
+        }
+      })
+    })
+  }
+
+  return (
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={8}>
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <CForm onSubmit={loginSubmit}>
+                    <h1>Авторизация</h1>
+                    <p className="text-medium-emphasis">Войдите в ваш аккаунт</p>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>
+                        <CIcon icon={cilUser} />
+                      </CInputGroupText>
+                      <CFormInput
+                        placeholder="Логин"
+                        name={'name'}
+                        onChange={handleInput}
+                        value={loginInput.name}
+                      />
+                    </CInputGroup>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>
+                        <CIcon icon={cilLockLocked} />
+                      </CInputGroupText>
+                      <CFormInput
+                        type="password"
+                        placeholder="Пароль"
+                        name={'password'}
+                        onChange={handleInput}
+                        value={loginInput.password}
+                      />
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <Select
+                        defaultValue={selectedOption}
+                        onChange={setSelectedOption}
+                        options={options}
+                        isMulti={true}
+                      />
+                    </CInputGroup>
+                    <CRow>
+                      <CCol xs={6}>
+                        <CButton type={'submit'} color="dark" variant="outline" className="px-4">
+                          Войти
+                        </CButton>
+                      </CCol>
+                    </CRow>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  )
+}
+
+export default Login
